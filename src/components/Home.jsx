a065f6d2e98c07f0371fc82138f9c011723c342f
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-20 21:35:25 -0800

    Add padding left and top to h6 text.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import "../styles/Home.css";
 export default function Home() {
   return (
     <div className="home">
-      <h6 className="home-title">Adele</h6>
+      <h6 className="home-title ps-5 pt-5">Adele</h6>
     </div>
   );
 }
