965c4abf24ed7eeded0185b6af0f93cc4570333c
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-22 16:19:03 -0800

    Add p tag with placeholder text.

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -3,8 +3,17 @@ import "../styles/Bio.css";
 
 export default function Bio() {
   return (
-    <div>
-      <h6 className="bio-title pt-5 pb-5">BIOGRAPHY</h6>
+    <div className="ms-5 me-5">
+        <h6 className="bio-title pt-5 pb-5">BIOGRAPHY</h6>
+        <p>
+          Cloud bread poke 90's literally, seitan mlkshk raw denim heirloom
+          asymmetrical chicharrones iceland. Chartreuse venmo humblebrag shaman.
+          Vinyl drinking vinegar deep v swag, cliche YOLO shaman hoodie four
+          loko poutine forage 8-bit neutra. Bespoke cray gentrify organic four
+          loko irony readymade pabst deep v bushwick flexitarian artisan
+          crucifix master cleanse. Celiac bodega boys artisan wayfarers sus
+          gluten-free try-hard scenester hella ugh synth slow-carb.
+        </p>
     </div>
   );
 }
