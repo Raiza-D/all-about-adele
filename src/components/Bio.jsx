f6bed32527ac2bee65e032c2bd9bd103511df022
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-23 22:18:06 -0800

    Remove placeholder text. Add text with info taken from Wikipedia.

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -6,13 +6,10 @@ export default function Bio() {
     <div className="ms-5 me-5">
         <h6 className="bio-title pt-5 pb-5">BIOGRAPHY</h6>
         <p>
-          Cloud bread poke 90's literally, seitan mlkshk raw denim heirloom
-          asymmetrical chicharrones iceland. Chartreuse venmo humblebrag shaman.
-          Vinyl drinking vinegar deep v swag, cliche YOLO shaman hoodie four
-          loko poutine forage 8-bit neutra. Bespoke cray gentrify organic four
-          loko irony readymade pabst deep v bushwick flexitarian artisan
-          crucifix master cleanse. Celiac bodega boys artisan wayfarers sus
-          gluten-free try-hard scenester hella ugh synth slow-carb.
+          Adele Laurie Adkins Blue, or more famously known by her stage name 'Adele', was born on May 5th 1988 in the Tottenham district of London. She began singing at the age of 4 and asserts that she became obsessed with voices. Adele attended BRIT School for Performing Arts & Technology in Croydon, where she was a classmate of recording artists Leona Lewis and Jessie J.
+
+          Adele's breakthrough song, "Hometown Glory", was written when she was 16. It is about the district of West Norwood in South London, where she lived for several years.
+
         </p>
     </div>
   );
