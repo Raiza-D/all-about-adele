733faa4e609567bf973b13dc9f03526479c4869b
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-23 22:19:22 -0800

    Add break tag.

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -7,7 +7,7 @@ export default function Bio() {
         <h6 className="bio-title pt-5 pb-5">BIOGRAPHY</h6>
         <p>
           Adele Laurie Adkins Blue, or more famously known by her stage name 'Adele', was born on May 5th 1988 in the Tottenham district of London. She began singing at the age of 4 and asserts that she became obsessed with voices. Adele attended BRIT School for Performing Arts & Technology in Croydon, where she was a classmate of recording artists Leona Lewis and Jessie J.
-
+          <br />
           Adele's breakthrough song, "Hometown Glory", was written when she was 16. It is about the district of West Norwood in South London, where she lived for several years.
 
         </p>
