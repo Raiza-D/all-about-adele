3831e1ca83137f343654d69e116f2ba108affc18
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-23 22:25:10 -0800

    Add another break tag. Add more bio content about Adele.

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -8,7 +8,10 @@ export default function Bio() {
         <p>
           Adele Laurie Adkins Blue, or more famously known by her stage name 'Adele', was born on May 5th 1988 in the Tottenham district of London. She began singing at the age of 4 and asserts that she became obsessed with voices. Adele attended BRIT School for Performing Arts & Technology in Croydon, where she was a classmate of recording artists Leona Lewis and Jessie J.
           <br />
-          Adele's breakthrough song, "Hometown Glory", was written when she was 16. It is about the district of West Norwood in South London, where she lived for several years.
+          <br />
+          Adele's breakthrough song, "Hometown Glory", was written when she was 16. It is about the district of West Norwood in South London, where she lived for several years. So far, Adele has four studio albums, with each one titled based on her age when the album was released. From her first studio hit of "Hometown Glory" to now, Adele has won 16 Grammy awards and has even won an Academy Award for her James Bond soundtrack single "Skyfall".
+
+
 
         </p>
     </div>
