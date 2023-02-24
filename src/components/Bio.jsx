6848b3350797443b53f1cbf14bf10ebfee0825b2
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-23 22:26:39 -0800

    Add another two break tags to make way for a third paragraph.

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -10,6 +10,8 @@ export default function Bio() {
           <br />
           <br />
           Adele's breakthrough song, "Hometown Glory", was written when she was 16. It is about the district of West Norwood in South London, where she lived for several years. So far, Adele has four studio albums, with each one titled based on her age when the album was released. From her first studio hit of "Hometown Glory" to now, Adele has won 16 Grammy awards and has even won an Academy Award for her James Bond soundtrack single "Skyfall".
+          <br />
+          <br />
 
 
 
