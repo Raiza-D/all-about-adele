996256b7ccdfa22d0399a117e7d6cc0a0ee1f85d
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-26 21:00:56 -0800

    Remove margin start and end from main div tag.

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -3,7 +3,7 @@ import "../styles/Bio.css";
 
 export default function Bio() {
   return (
-    <div className="ms-5 me-5">
+    <div>
       <h6 className="bio-title pt-5 pb-5">BIOGRAPHY</h6>
       <p>
         Adele Laurie Adkins Blue, or more famously known by her stage name
