694660bbaf8eb4a271ba342f525f03dcf5f9fe35
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-26 20:52:51 -0800

    Place second paragraph in its own p tag.

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,17 +4,31 @@ import "../styles/Bio.css";
 export default function Bio() {
   return (
     <div className="ms-5 me-5">
-        <h6 className="bio-title pt-5 pb-5">BIOGRAPHY</h6>
+      <h6 className="bio-title pt-5 pb-5">BIOGRAPHY</h6>
+      <p>
+        Adele Laurie Adkins Blue, or more famously known by her stage name
+        'Adele', was born on May 5th 1988 in the Tottenham district of London.
+        She began singing at the age of 4 and asserts that she became obsessed
+        with voices. Adele attended BRIT School for Performing Arts & Technology
+        in Croydon, where she was a classmate of recording artists Leona Lewis
+        and Jessie J.
         <p>
-          Adele Laurie Adkins Blue, or more famously known by her stage name 'Adele', was born on May 5th 1988 in the Tottenham district of London. She began singing at the age of 4 and asserts that she became obsessed with voices. Adele attended BRIT School for Performing Arts & Technology in Croydon, where she was a classmate of recording artists Leona Lewis and Jessie J.
-          <br />
-          <br />
-          Adele's breakthrough song, "Hometown Glory", was written when she was 16. It is about the district of West Norwood in South London, where she lived for several years. So far, Adele has four studio albums, with each one titled based on her age when the album was released. From her first studio hit of "Hometown Glory" to now, Adele has won 16 Grammy awards and has even won an Academy Award for her James Bond soundtrack single "Skyfall".
-          <br />
-          <br />
-          British pop band, Spice Girls, has been a major influence in regards to Adele's love and passion for music, stating that "they made me what I am today." Growing up, Adele listened to Sinead O'Connor, the Cranberries, Bob Marley, Whitney Houston, Aretha Franklin, and Celine Dion to name a few.
-
+          Adele's breakthrough song, "Hometown Glory", was written when she was
+          16. It is about the district of West Norwood in South London, where
+          she lived for several years. So far, Adele has four studio albums,
+          with each one titled based on her age when the album was released.
+          From her first studio hit of "Hometown Glory" to now, Adele has won 16
+          Grammy awards and has even won an Academy Award for her James Bond
+          soundtrack single "Skyfall".
         </p>
+        <br />
+        <br />
+        British pop band, Spice Girls, has been a major influence in regards to
+        Adele's love and passion for music, stating that "they made me what I am
+        today." Growing up, Adele listened to Sinead O'Connor, the Cranberries,
+        Bob Marley, Whitney Houston, Aretha Franklin, and Celine Dion to name a
+        few.
+      </p>
     </div>
   );
 }
