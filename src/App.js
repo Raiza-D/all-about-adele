fcdb57df48685f93d8888fafb44a988a120e970b
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-26 21:13:26 -0800

    Import Albums component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from "./components/Home.jsx";
 import Bio from "./components/Bio.jsx";
+import Albums from "./components/Albums.jsx";
 
 function App() {
   return (
