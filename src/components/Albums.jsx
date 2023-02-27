2750de97b0358b30f4402f498e0d11d757ccad11
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-26 21:42:52 -0800

    Add two more Accordion Items.

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -5,7 +5,9 @@ import Accordion from "react-bootstrap/Accordion";
 export default function Albums() {
   return (
     <div className="albums-container">
-      <h6 className="text-center fw-bold pt-5 pb-5 albums-title">DISCOGRAPHY</h6>
+      <h6 className="text-center fw-bold pt-5 pb-5 albums-title">
+        DISCOGRAPHY
+      </h6>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
           <Accordion.Header>Accordion Item #1</Accordion.Header>
@@ -16,6 +18,20 @@ export default function Albums() {
         </Accordion.Item>
         <Accordion.Item eventKey="1">
           <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>25</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>25</Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
