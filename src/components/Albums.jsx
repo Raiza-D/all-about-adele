1d5854ac5d4a55df8a61537854ea06d4ffb29eff
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-27 21:38:33 -0800

    Add img tag to display 30 album cover image.

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -11,7 +11,10 @@ export default function Albums() {
       </h6>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
-          <Accordion.Header>30 | 2021</Accordion.Header>
+          <Accordion.Header>
+            <img src={fourthAlbum} />
+            30 | 2021
+          </Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
