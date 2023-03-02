2b2591c9ad5eb5d0ab8ceda371e27e5d957acacd
Raiza De Guzman <santos_dg7@hotmail.com>
2023-03-01 22:59:48 -0800

    Add img tag using 21 album photo.

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -45,7 +45,9 @@ export default function Albums() {
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
-          <Accordion.Header>21 | 2011</Accordion.Header>
+          <Accordion.Header>
+            <img src={secondAlbum} />
+            21 | 2011</Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
